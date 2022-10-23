8dbf2a11536854197a7e5d8a96ef76411aa987e7
Soidnarg <rubenbagnod@gmail.com>
2022-10-23 13:11:25 +0200

    Step update with unworking gsm, it goes shutdown for an hardware issue

--- gsmSerial.ino
+++ gsmSerial.ino
@@ -2,7 +2,9 @@
 #include <avr/wdt.h>
 #include <SoftwareSerial.h>
 
-#define Reset_AVR() wdt_enable(WDTO_30MS); while(1) {} //definizione della funzione reset
+//#define Reset_AVR() wdt_enable(WDTO_30MS); while(1) {} //definizione della funzione reset
+
+#define TYPICAL_DELAY 25
 
 #define ERR_NO_OK 0
 #define ERR_PASSWORD_REQUIRED 1
@@ -11,9 +13,40 @@
 
 int InitGsm();
 String ReadAllSms();
-String WriteSms(String da, String message);
+int WriteSms(String da, String message);
 String ReadThings();
 
+typedef struct Gestore{  
+  String name;
+  String apn;
+  String mmscMms;
+  String mmscInternet;
+  int mcc;
+  int mnc;
+  String apnTypeMms;
+  String apnTypeInternet;
+  String apnProtocol;
+  String apnProtocolRoaming;
+  String operatorType;
+  String valueVirtualOperator;    
+  String sms;  
+} Gestore;
+
+Gestore Iliad = {
+  "Iliad",
+  "Iliad",
+  "http://mms.iliad.it",
+  "http://mms.iliad.it",
+  222,
+  50,
+  "default, supl, mms, dun",
+  "default, supl, mms, dun",
+  "Ipv4",
+  "Ipv4",
+  "gid",
+  "F003",
+  "+393519999600",
+};
 
 SoftwareSerial mySerial(2, 3);
 
@@ -22,16 +55,25 @@ void setup() {
   // put your setup code here, to run once:
   mySerial.begin(9600);
   Serial.begin(9600);
-  delay(10000); 
+  delay(5000); 
+/*  
   ris = InitGsm();
   if(ris != ITSOK) {
     Serial.println(ris);
     Reset_AVR();
   }
+*/
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while(mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  
+  while(Serial.available()) {
+    mySerial.write(Serial.read());
+  }
 }
 
 String ReadThings() {
@@ -45,7 +87,7 @@ String ReadThings() {
 int InitGsm() {
   String answer;
   mySerial.println("ATE0");
-  delay(25);
+  delay(TYPICAL_DELAY);
   answer = ReadThings();
   if(answer.indexOf("\r\nOK") == -1) {
     return ERR_NO_OK;  
@@ -61,33 +103,65 @@ int InitGsm() {
   }
 
   mySerial.println("AT+CMGF=1");
-  delay(25);
+  delay(TYPICAL_DELAY);
   answer = ReadThings();
   if(answer.indexOf("\r\nOK") == -1) {
     return ERR_NO_OK;  
   }
 
-  
+  mySerial.println("AT+CSCA=\"" + Iliad.sms + "\"\r");
+  delay(TYPICAL_DELAY);
+  answer = ReadThings();
+  if(answer.indexOf("\r\nOK") == -1) {
+    return ERR_NO_OK;  
+  }
+
+  mySerial.println("AT+CGATT=1\r");
+  delay(TYPICAL_DELAY);
+  answer = ReadThings();
+  if(answer.indexOf("\r\nOK") == -1) {
+    return ERR_NO_OK;  
+  }
     
+  mySerial.println("AT+CGACT=1\r");
+  delay(TYPICAL_DELAY);
+  answer = ReadThings();
+  if(answer.indexOf("\r\nOK") == -1) {
+    return ERR_NO_OK;  
+  }
+    
+  mySerial.println("AT+CGREG=2\r");
+  delay(TYPICAL_DELAY);
+  answer = ReadThings();
+  if(answer.indexOf("\r\nOK") == -1) {
+    return ERR_NO_OK;  
+  }
+  
   return ITSOK;
 }
 
 String ReadAllSms() {
   mySerial.println("AT+CMGL=\"ALL\", 1");
-  delay(25);
+  delay(TYPICAL_DELAY);
   return ReadThings();
 }
 
-String WriteSms(String da, String message) {
+int WriteSms(String da, String message) {
   String answer;
 
-  mySerial.println("AT+CMGS=\"%s\"\r", da);
-  delay(25);  
+  mySerial.println("AT+CMGS=\"" + da + "\"\r");
+  delay(TYPICAL_DELAY);
+  answer = ReadThings();
   if(answer.indexOf(">") == -1) {
     return ERR_NO_OK;
   } else {
-    mySerial.println("%s%c", message, 0x1A);
-    delay(25);
-    return ReadThings;
+    mySerial.println(message + 0x1A);
+    delay(TYPICAL_DELAY);
+    answer = ReadThings();
+    if(answer.indexOf("\r\nOK") == -1) {
+      return ERR_NO_OK;
+    } else {
+      return sizeof(message);
+    }
   }
 }
